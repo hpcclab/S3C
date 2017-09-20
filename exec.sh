d312c95074ad5a917ba6e7f3c5847399c9866371
zobaed11 <31321339+zobaed11@users.noreply.github.com>
2017-09-20 00:13:35 -0500

    Add files via upload

---
+++ exec.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+a = 'pwd'
+CLIENT_BUILD_DIR=  $a/cloud1
+if [ ! -d "$CLIENT_BUILD_DIR" ]
+then mkdir "$CLIENT_BUILD_DIR"cloud1
+mkdir $a"cloud1/cloudserver"
+cd $a"cloud1"
+cd cloudserver
+mkdir storage utilities watch
+fi
